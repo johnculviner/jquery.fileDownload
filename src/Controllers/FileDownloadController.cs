d7f8e9fbb87057d712444fb54b9bfe4be32630c7
John Culviner <john.culviner@gmail.com>
2012-06-13 21:02:56 -0500

    added support for mobile browsers (v1.3.0) and added MVC Demo Project

---
+++ src/Controllers/FileDownloadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.Common;
+
+namespace MvcDemo.Controllers
+{
+    public class FileDownloadController : FileDownloadControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public FilePathResult DownloadReport(int id)
+        {
+            return GetReport(id);
+        }
+
+        [HttpPost]
+        public FilePathResult DownloadReportPost(int foo)
+        {
+            return GetReport(foo);
+        }
+
+        private FilePathResult GetReport(int id)
+        {
+            //simulate generating the report
+            Thread.Sleep(3000);
+
+            //only even file ids will work
+            if (id % 2 == 0)
+                //the required cookie for jquery.fileDownload is written by FileDownloadController for all
+                //result types that inherit from FileResult but could be done manually here if desired
+                return File("~/Report.pdf", "application/pdf", string.Format("Report{0}.pdf", id));
+
+
+            throw new Exception(string.Format("File Report{0}.pdf could not be found. \r\n\r\n NOTE: This is for demonstration purposes only, customErrors should always be enabled in a production environment.", id));
+        }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvcDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc531874-ef2b-4148-b402-b820d8e4b11d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
